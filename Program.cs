67a3c20cd34b78840d9e094e5d3676cbe5d5ac15
EthanMcBloxxer <39684348+EthanMcBloxxer@users.noreply.github.com>
2021-03-23 10:22:48 -0400

    Add project files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Creverse
+{
+    class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Creverse (Console Reverse) by EthanMcBloxxer under the Unlicense");
+            Console.WriteLine();
+            while (true)
+            {
+                Console.Write("Creverse>");
+                string x = Console.ReadLine();
+                char[] xa = x.ToCharArray();
+                Array.Reverse(xa);
+                string y = "";
+                foreach (char tmp in xa) {y += tmp;}
+                try
+                {
+                    Clipboard.SetText(y);
+                    Console.WriteLine(y + " (copied)");
+                }
+                catch
+                {
+                    Console.WriteLine(y);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Creverse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Creverse")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cfd63798-ebe4-46fb-bd16-dfbfe1c448ba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
